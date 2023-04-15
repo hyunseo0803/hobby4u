2fbe58a8f070bd04919012e7e30d189cd4dc90ae
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-15 15:04:34 +0900

    💄 Footer  UI 및 CSS 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "../styles/Header.css";
 import { Link } from "react-router-dom";
+import "../styles/Footer.css";
 
 function Footer(props) {
 	return (
 		<div className="footer_wrap">
-			<Link to="testinfo">
-				<p>이곳은 footer 입니다. 관리자 정보 보고싶으면 누르세요</p>
-			</Link>
+			<div className="footer">문의: hobby4uContact@gmail.com</div>
 		</div>
 	);
 }
