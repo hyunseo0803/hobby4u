96dd6ab9fc6e5e42fe642d7832d702c203553e0d
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-15 15:07:04 +0900

    💄 헤더 UI 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,45 @@
 import React from "react";
 import "../styles/Header.css";
 import { Link } from "react-router-dom";
+import Logo from "../assets/Logo.png";
 
 function Header(props) {
 	return (
-		<div className="header_wrap">
-			<Link to="/">
-				<p>헤더 이고, 맨 처음 화면으로 감 </p>
-			</Link>
-			<Link to="test">
-				<p>헤더 이고, 헤더로 인해 바뀐 화면으로 감 </p>
-			</Link>
+		<div className="header_wrapper">
+			<div className="logo_wrapper">
+				<Link to="/">
+					<img className="logo" alt="logo" src={Logo} />
+				</Link>
+			</div>
+			<div className="menu_wrapper">
+				<div className="menu_item">
+					<Link to="intro" className="link">
+						소개 및 이용방법
+					</Link>
+				</div>
+				<div className="menu_item">
+					<Link to="test" className="link">
+						모든 클래스
+					</Link>
+				</div>
+				<div className="menu_item">
+					<Link to="test" className="link">
+						클래스 찾기
+					</Link>
+				</div>
+				<div className="menu_item">
+					<Link to="test" className="link">
+						클래스 만들기
+					</Link>
+				</div>
+			</div>
+			<div className="login_wrapper">
+				<div className="login_item">
+					<Link to="test" className="link">
+						로그인/회원가입
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 }
