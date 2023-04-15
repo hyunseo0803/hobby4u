3ba355290b4811d2d775f53b9a767352e49dd9b1
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-15 15:08:25 +0900

    💄 메인 UI 추가

--- src/pages/FirstMain.js
+++ src/pages/FirstMain.js
@@ -1,9 +1,51 @@
 import React from "react";
+import Home from "../../src/assets/Home.png";
+import "../styles/FirstMain.css";
 
 function FirstMain(props) {
 	return (
-		<div className="Main_wrap" style={{ backgroundColor: "red" }}>
-			<h1>메인 입니다. </h1>
+		<div className="Main_wrap">
+			<img src={Home} alt="" className="homeImg" />
+			<div className="home_text_left">
+				<span>
+					나<br></br>
+				</span>
+				<span>
+					만<br></br>
+				</span>
+				<span>
+					을<br></br>
+				</span>
+				<span>
+					<br></br>
+				</span>
+				<span>
+					위<br></br>
+				</span>
+				<span>
+					한<br></br>
+				</span>
+			</div>
+			<div className="home_text_right">
+				<span>
+					완<br></br>
+				</span>
+				<span>
+					벽<br></br>
+				</span>
+				<span>
+					한<br></br>
+				</span>
+				<span>
+					<br></br>
+				</span>
+				<span>
+					취<br></br>
+				</span>
+				<span>
+					미<br></br>
+				</span>
+			</div>
 		</div>
 	);
 }
