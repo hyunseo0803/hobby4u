5e0f52e843b6339eeb220c67aac34d7a70bfed08
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-15 15:02:43 +0900

    ✨ 소개 및 이용방법 UI 추가_Intro.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FirstMain from "./pages/FirstMain";
-import Test from "./pages/Test";
+import Intro from "./pages/Intro";
 import TestInfo from "./pages/TestInfo";
+import "../src/App.css";
+
 const App = () => {
 	/*const heaer_wrap= {
 		backgroundColor: "yellow"
 	}*/
+
 	return (
 		<BrowserRouter>
-			<div className="App">
+			<div className="Container">
 				<Header />
 				{/* <FirstMain /> */}
 				<Routes>
 					<Route path="/" element={<FirstMain />} />
-					<Route path="test" element={<Test />} />
+					<Route path="intro" element={<Intro />} />
 					<Route path="testinfo" element={<TestInfo />} />
 				</Routes>
 				<Footer />
