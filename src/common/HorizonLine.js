222d3b8c90184ef76f5c6b50aeb55703cb85505c
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-16 17:13:28 +0900

    🎨 style 분리

--- src/common/HorizonLine.js
+++ src/common/HorizonLine.js
@@ -1,17 +1,22 @@
 import React from "react";
-
+//수평 구분선 안의 글자 props
 const HorizonLine = ({ text }) => {
+	//props 글자의 기본 style
+	const TextStyle = {
+		background: "#fff",
+		padding: "0 10px",
+	};
+
+	const DivStyle = {
+		width: "100%",
+		textAlign: "center",
+		borderBottom: "1px solid #aaa",
+		lineHeight: "0.1em",
+		margin: "10px 0 20px",
+	};
 	return (
-		<div
-			style={{
-				width: "100%",
-				textAlign: "center",
-				borderBottom: "1px solid #aaa",
-				lineHeight: "0.1em",
-				margin: "10px 0 20px",
-			}}
-		>
-			<span style={{ background: "#fff", padding: "0 10px" }}>{text}</span>
+		<div style={DivStyle}>
+			<span style={TextStyle}>{text}</span>
 		</div>
 	);
 };
