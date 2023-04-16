7a9efb24a1d3fa11b7d2a043bfb841eb5978a310
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-16 17:11:57 +0900

    🔥 TestInfo 테스트 파일 삭제 및 불필요한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FirstMain from "./pages/FirstMain";
 import Intro from "./pages/Intro";
-import TestInfo from "./pages/TestInfo";
 import "../src/App.css";
 
 const App = () => {
-	/*const heaer_wrap= {
-		backgroundColor: "yellow"
-	}*/
-
 	return (
 		<BrowserRouter>
 			<div className="Container">
@@ -20,7 +15,6 @@ const App = () => {
 				<Routes>
 					<Route path="/" element={<FirstMain />} />
 					<Route path="intro" element={<Intro />} />
-					<Route path="testinfo" element={<TestInfo />} />
 				</Routes>
 				<Footer />
 			</div>
