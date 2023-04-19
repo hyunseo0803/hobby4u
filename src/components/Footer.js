9dfcb719baa29186f08d7c1c4e13705e76e22619
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-19 15:41:54 +0900

    💄 Footer UI 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,27 @@ import "../styles/Footer.css";
 function Footer(props) {
 	return (
 		<div className="footer_wrap">
-			<div className="footer">문의: hobby4uContact@gmail.com</div>
+			<div className="footer">
+				<div className="footer_title">Introduction</div>
+				<div className="footer_content">
+					You can see at a glance how to use hobby4u and the benefits you can
+					enjoy on our site.
+				</div>
+			</div>
+			<div className="footer">
+				<div className="footer_title">Introduction</div>
+				<div className="footer_content">
+					You can see at a glance how to use hobby4u and the benefits you can
+					enjoy on our site.
+				</div>
+			</div>
+			<div className="footer">
+				<div className="footer_title">Introduction</div>
+				<div className="footer_content">
+					You can see at a glance how to use hobby4u and the benefits you can
+					enjoy on our site.
+				</div>
+			</div>
 		</div>
 	);
 }
