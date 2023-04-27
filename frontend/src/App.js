2969db7dba453c8bc525e1d295aa270b72374ba4
hyunseo0803 <choihyunseo135@gmail.com>
2023-04-27 14:36:41 +0900

    ✅ DB의 데이터 불러오기 테스트 성공

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		members: [],
+	};
+	async componentDidMount() {
+		try {
+			const res = await fetch("http://127.0.0.1:8000/api/");
+			const members = await res.json();
+			this.setState({
+				members,
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	render() {
+		return (
+			<div className="App">
+				{this.state.members.map((item) => (
+					<div key={item.id}>
+						<h1>{item.email}</h1>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
