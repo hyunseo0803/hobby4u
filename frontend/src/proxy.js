38d3ac4f65a96792df6717e2cd05af3282b39ba9
hyunseo0803 <choihyunseo135@gmail.com>
2023-05-07 20:38:51 +0900

    cors에러 해결중

---
+++ frontend/src/proxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+	app.use(
+		createProxyMiddleware("/auth", {
+			target: "http://localhost:8000/",
+			changeOrigin: true,
+			// pathRewrite: {
+			// 	"^/kakao": "",
+			// },
+		})
+	);
+};
