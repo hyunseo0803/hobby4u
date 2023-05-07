a653674050722b9bed071f7d663d79bfb276ab9d
hyunseo0803 <choihyunseo135@gmail.com>
2023-05-07 20:50:34 +0900

    카카오 로그인 경로 추가

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from .views import KakaoSignUpView
+from .views import KakaoCallbackView
+# from .views import check_login_status
 
 urlpatterns = [
-    path('',views.MemberList.as_view()),
-    path('<int:pk>/',views.MemberDetail.as_view())
+    # path('',views.MemberList.as_view()),
+    # path('<int:pk>/',views.MemberDetail.as_view()),
+    path('kakao/callback/',KakaoCallbackView.as_view()),
+    path('kakao/login/',KakaoSignUpView.as_view()),
+    path('get_user_data/',views.get_user_data)
+    
+    # path('/api/check_login_status/',check_login_status),
 ]
