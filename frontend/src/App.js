573e10b1ea266755f759359290c494208aaba0b4
hyunseo0803 <choihyunseo135@gmail.com>
2023-06-17 19:16:37 +0900

    ✨ FEAT : 사용자 메뉴 페이지 Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Intro from "./pages/Intro";
 import "../src/App.css";
+import Setting from "./pages/Setting";
+import Myclass from "./pages/Myclass";
+import Likeclass from "./pages/Likeclass";
+import Exam from "./pages/Exam";
 
 class App extends Component {
 	render() {
@@ -14,7 +18,12 @@ class App extends Component {
 					{/* <FirstMain /> */}
 					<Routes>
 						<Route path="intro" element={<Intro />} />
+						<Route path="setting" element={<Setting />} />
+						<Route path="myclass" element={<Myclass />} />
+						<Route path="likeclass" element={<Likeclass />} />
+						<Route path="exam" element={<Exam />} />
 					</Routes>
+
 					<Footer />
 				</div>
 			</BrowserRouter>
