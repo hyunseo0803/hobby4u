7532b1c58763ccc305b2d77b2d15d5968d5de000
hyunseo0803 <abcdefghjk5@naver.com>
2023-09-24 22:11:41 +0900

    ✨ FEAT: 사진인지 아닌지 확인 후 이미지 혹은 영상 가져옴
    🐛 FIX: 이미지 클릭시 에러 FIX
    
    💄 STYLES: 유료/무료, 온라인/오프라인 UI 추가 및 전체적인 UI 수정
    
     ✨ FEAT: 작성자 정보 가져옴

--- frontend/src/pages/ReadClass/FilterClass.js
+++ frontend/src/pages/ReadClass/FilterClass.js
@@ -1,98 +1,212 @@
 import "../../styles/ReadClass.css";
+import ReadClassOptionLB from "../../component/AllReadOptionLB";
 
 export default function FILTER_CLASS(props) {
-	const {
-		option,
-		money,
-		apply_ok,
-		data,
-		handleReadDetail,
-		calculateDaysLeft,
-		fliteredata,
-	} = props;
+	const { option, money, apply_ok, data, handleReadDetail, fliteredata } =
+		props;
+
+	function isImage(urlString) {
+		const fileEx = urlString.split(".").pop().toLowerCase();
+
+		const imageEx = ["jpg", "jpeg", "png", "gif", "bmp", "webp", "svg"];
+
+		return imageEx.includes(fileEx);
+	}
 
 	return (
-		<>
+		<div className="row_center_wrap">
 			{option === "" && money === "" && apply_ok === false ? (
-				data.map((classItem, index) => (
-					<>
+				data.map((classItem, index) => {
+					const firstimg = classItem.img.replace("/frontend/public/", "/");
+
+					const handleImageClick = (e) => {
+						e.stopPropagation();
+						handleReadDetail(classItem.class_id);
+					};
+
+					const handleButtonClick = () => {
+						handleReadDetail(classItem.class_id);
+					};
+					const isFree = classItem.money === "0";
+					const isOnline = classItem.type === "online";
+
+					return (
+						// <div key={index} className="class_div_btn">
 						<button
 							key={index}
 							className="class_div_btn"
 							value={classItem.class_id}
-							onClick={(e) => handleReadDetail(e.target.value)}
+							onClick={handleButtonClick}
 						>
-							<strong>Title:</strong>
-							{classItem.title}
-							<br />
-							<strong>Info:</strong>{" "}
+							<div className="firstimg_container">
+								{isImage(classItem.img) ? (
+									<img
+										className="firstimg"
+										src={firstimg}
+										alt="gg"
+										onClick={handleImageClick}
+									/>
+								) : (
+									<video
+										className="firstimg"
+										src={firstimg}
+										alt="gg"
+										onClick={handleImageClick}
+										controls
+									/>
+								)}
+							</div>
 							<div
+								className="class_div_MO"
 								style={{
-									overflow: "hidden",
-									textOverflow: "ellipsis",
-									whiteSpace: "nowrap",
+									justifyContent: "space-between",
+									alignItems: "center",
 								}}
 							>
-								{classItem.info}
+								<ReadClassOptionLB isFree={isFree} isOnline={isOnline} />
+								<div className="class_GCount">
+									<img
+										width="25"
+										height="25"
+										src="https://img.icons8.com/ios/30/f26b6b/like--v1.png"
+										alt="like--v1"
+									/>
+									<div style={{ fontSize: 16, width: 20, color: "#F26B6B" }}>
+										{classItem.goodCount}
+									</div>
+								</div>
 							</div>
-							<br />
-							{/* <strong>마감 시작:</strong> {classItem.applystart}
-        <br />
-        <strong>마감 마감:</strong> {classItem.applyend}
-        <br /> */}
-							{calculateDaysLeft(classItem.applyend) > 0 ? (
-								<strong>
-									신청 마감 디데이:{calculateDaysLeft(classItem.applyend)}일
-									남음
-								</strong>
-							) : (
-								<strong>신청 기간이 아닙니다</strong>
-							)}
+							<div
+								style={{
+									textAlign: "start",
+									justifyContent: "flex-start",
+									left: 0,
+									marginTop: 5,
+								}}
+							>
+								{classItem.title}
+							</div>
+							<div className="row_center_wrap">
+								<img
+									src={classItem.id.profile}
+									alt="profile"
+									width={30}
+									height={30}
+									style={{ borderRadius: "50%" }}
+								/>
+								<div
+									style={{
+										alignItems: "center",
+										justifyContent: "center",
+										width: "90%",
+										textAlign: "left",
+										padding: 4,
+									}}
+								>
+									{classItem.id.nickname}
+								</div>
+							</div>
 						</button>
-					</>
-				))
+					);
+				})
 			) : (
 				<div className="row_center_wrap">
 					{fliteredata &&
-						fliteredata.map((filter, index) => (
-							<>
+						fliteredata.map((filter, index) => {
+							const firstimg = filter.img.replace("/frontend/public/", "/");
+
+							const handleImageClick = (e) => {
+								e.stopPropagation();
+								handleReadDetail(filter.class_id);
+							};
+							const handleButtonClick = () => {
+								handleReadDetail(filter.class_id);
+							};
+							const isFree = filter.money === "0";
+							const isOnline = filter.type === "online";
+
+							return (
 								<button
 									key={index}
 									className="class_div_btn"
 									value={filter.class_id}
-									onClick={(e) => handleReadDetail(e.target.value)}
+									onClick={handleButtonClick}
 								>
-									<strong>Title:</strong>
-									{filter.title}
-									<br />
-									<strong>Info:</strong>{" "}
+									<div className="firstimg_container">
+										{isImage(filter.img) ? (
+											<img
+												className="firstimg"
+												src={firstimg}
+												alt="gg"
+												onClick={handleImageClick}
+											/>
+										) : (
+											<video
+												className="firstimg"
+												src={firstimg}
+												alt="gg"
+												onClick={handleImageClick}
+												controls
+											/>
+										)}
+									</div>
 									<div
+										className="class_div_MO"
 										style={{
-											overflow: "hidden",
-											textOverflow: "ellipsis",
-											whiteSpace: "nowrap",
+											justifyContent: "space-between",
+											alignItems: "center",
 										}}
 									>
-										{filter.info}
+										<ReadClassOptionLB isFree={isFree} isOnline={isOnline} />
+										<div className="class_GCount">
+											<img
+												width="25"
+												height="25"
+												src="https://img.icons8.com/ios/30/f26b6b/like--v1.png"
+												alt="like--v1"
+											/>
+											<div
+												style={{ fontSize: 16, width: 20, color: "#F26B6B" }}
+											>
+												{filter.goodCount}
+											</div>
+										</div>
 									</div>
-									<br />
-									{/* <strong>마감 시작:</strong> {classItem.applystart}
-        <br />
-        <strong>마감 마감:</strong> {classItem.applyend}
-        <br /> */}
-									{calculateDaysLeft(filter.applyend) > 0 ? (
-										<strong>
-											신청 마감 디데이: {calculateDaysLeft(filter.applyend)}일
-											남음
-										</strong>
-									) : (
-										<strong>신청 기간이 아닙니다</strong>
-									)}
+									<div
+										style={{
+											textAlign: "start",
+											justifyContent: "flex-start",
+											left: 0,
+											marginTop: 5,
+										}}
+									>
+										{filter.title}
+									</div>
+									<div className="row_center_wrap">
+										<img
+											src={filter.id.profile}
+											alt="profile"
+											width={30}
+											height={30}
+											style={{ borderRadius: "50%" }}
+										/>
+										<div
+											style={{
+												alignItems: "center",
+												justifyContent: "center",
+												width: "90%",
+												textAlign: "left",
+												padding: 4,
+											}}
+										>
+											{filter.id.nickname}
+										</div>
+									</div>
 								</button>
-							</>
-						))}
+							);
+						})}
 				</div>
 			)}
-		</>
+		</div>
 	);
 }
