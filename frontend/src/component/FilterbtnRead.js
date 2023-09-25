e951cd9c2899bff5ed92244f66b6a19a87e20708
hyunseo0803 <abcdefghjk5@naver.com>
2023-09-25 19:59:29 +0900

    💄 STYLES: 필터 버튼 Hover 구현 완료

--- frontend/src/component/FilterbtnRead.js
+++ frontend/src/component/FilterbtnRead.js
@@ -14,58 +14,45 @@ export default function FILTER_BTN(props) {
 			<div
 				className="flex_row"
 				style={{
-					// backgroundColor: "yellow",
 					padding: 10,
 				}}
 			>
 				<div style={{ marginRight: 20 }}>
 					<button
-						className="btn_normal"
+						className={money === "fee" ? "btn_normal_click" : "btn_normal"}
 						name="money"
 						value="fee"
 						onClick={(e) => addFilter(e)}
-						style={{
-							backgroundColor: money === "fee" ? "pink" : "transparent",
-						}}
 					>
 						유료
 					</button>
 				</div>
 				<div style={{ marginRight: 20 }}>
 					<button
-						className="btn_normal "
+						className={money === "free" ? "btn_normal_click" : "btn_normal"}
 						name="money"
 						value="free"
 						onClick={(e) => addFilter(e)}
-						style={{
-							backgroundColor: money === "free" ? "pink" : "transparent",
-						}}
 					>
 						무료
 					</button>
 				</div>
 				<div style={{ marginRight: 20 }}>
 					<button
-						className="btn_normal"
+						className={option === "offline" ? "btn_normal_click" : "btn_normal"}
 						name="option"
 						value="offline"
 						onClick={(e) => addFilter(e)}
-						style={{
-							backgroundColor: option === "offline" ? "pink" : "transparent",
-						}}
 					>
 						오프라인
 					</button>
 				</div>
 				<div>
 					<button
-						className="btn_normal"
+						className={option === "online" ? "btn_normal_click" : "btn_normal"}
 						name="option"
 						value="online"
 						onClick={(e) => addFilter(e)}
-						style={{
-							backgroundColor: option === "online" ? "pink" : "transparent",
-						}}
 					>
 						온라인
 					</button>
@@ -74,17 +61,13 @@ export default function FILTER_BTN(props) {
 			<div
 				className="flex_row"
 				style={{
-					// backgroundColor: "yellow",
 					padding: 10,
 				}}
 			>
 				<button
-					className="btn_normal"
+					className={apply_ok ? "btn_normal_click" : "btn_normal"}
 					value="apply"
 					onClick={(e) => addFilter(e)}
-					style={{
-						backgroundColor: apply_ok ? "pink" : "transparent",
-					}}
 				>
 					신청 가능한 클래스
 				</button>
