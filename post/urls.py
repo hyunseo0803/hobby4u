6bf1e3a70916a2758482e0492aa10e10bc6b0dc3
hyunseo0803 <abcdefghjk5@naver.com>
2023-09-26 23:06:35 +0900

    🚚 PATH: 좋아요 기능 백엔드 경로 설정

--- post/urls.py
+++ post/urls.py
@@ -3,13 +3,14 @@ from . import views
 # from .views import KakaoCallbackView
 # from .views import get_user_data
 # from .views import check_login_status
-
+ 
 urlpatterns = [
     path('submit_data/',views.submit_data),
     path('read_all_data/',views.read_all_data),
     path('read_new_data/',views.read_new_data),
     path('read_some_data/',views.read_some_data),
     path('read_filter_data/',views.read_filter_data),
+    path('create_goodCount_data/',views.create_goodCount_data),
   
 
     # path('',views.MemberList.as_view()),
