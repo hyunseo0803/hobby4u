6d2d83b71d9dc8f07c3d7f82e36c7599fda3aced
hyunseo0803 <abcdefghjk5@naver.com>
2023-10-09 20:03:58 +0900

    ♻️ REFACTOR: 파일 분리 리팩토링

--- frontend/src/pages/ReadClass/FilterClass.js
+++ frontend/src/pages/ReadClass/FilterClass.js
@@ -1,14 +1,15 @@
 import "../../styles/ReadClass.css";
 import ReadClassOptionLB from "../../component/AllReadOptionLB";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function FILTER_CLASS(props) {
 	const {
 		option,
 		money,
 		apply_ok,
-		data,
-		readAll,
+		theme,
+		word,
+		readWordFilter,
 		handleReadDetail,
 		fliteredata,
 		like_status,
@@ -25,95 +26,18 @@ export default function FILTER_CLASS(props) {
 	}
 
 	useEffect(() => {
-		if (option === "" && money === "" && apply_ok === false) {
-			readAll();
-		} else {
-			readFilter();
+		if (option !== "" || money !== "" || apply_ok === false) {
+			if (theme !== "") {
+				readFilter(theme);
+			} else if (word !== "") {
+				readWordFilter(word);
+			} else readFilter();
 		}
-	}, [option, money, apply_ok]);
+	}, [option, money, apply_ok, theme, word]);
 
 	return (
-		<div className="row_center_wrap">
-			{option === "" && money === "" && apply_ok === false ? (
-				data.map((classItem, index) => {
-					const firstimg = classItem.img.replace("/frontend/public/", "/");
-
-					const handleImageClick = (e) => {
-						e.stopPropagation();
-						handleReadDetail(classItem.class_id);
-					};
-
-					const handleButtonClick = () => {
-						handleReadDetail(classItem.class_id);
-					};
-					const isFree = classItem.money === "0";
-					const isOnline = classItem.type === "online";
-
-					const likeStatusItem = like_status
-						? like_status.find((item) => item.class_id === classItem.class_id)
-						: null;
-
-					return (
-						<div key={index} className="class_div_btn">
-							<div className="firstimg_container">
-								{isImage(classItem.img) ? (
-									<img
-										className="firstimg"
-										src={firstimg}
-										alt="gg"
-										onClick={handleImageClick}
-									/>
-								) : (
-									<video
-										className="firstimg"
-										src={firstimg}
-										alt="gg"
-										onClick={handleImageClick}
-										controls
-									/>
-								)}
-							</div>
-							<div
-								className="class_div_MO"
-								style={{
-									justifyContent: "space-between",
-									alignItems: "center",
-								}}
-							>
-								<ReadClassOptionLB isFree={isFree} isOnline={isOnline} />
-								<div className="class_GCount">
-									<button
-										className="like_btn"
-										onClick={() => goodClick(classItem.class_id)}
-									>
-										{likeStatusItem ? "❤️" : "🤍"} 좋아요
-									</button>
-									<div className="like_text">{classItem.goodCount}</div>
-								</div>
-							</div>
-							<button
-								value={classItem.class_id}
-								onClick={handleButtonClick}
-								className="class_title_btn"
-							>
-								{classItem.title}
-							</button>
-							<div className="row_center_wrap">
-								<img
-									src={classItem.id.profile}
-									alt="profile"
-									width={30}
-									height={30}
-									style={{ borderRadius: "50%" }}
-								/>
-								<div className="class_nickname_btn">
-									{classItem.id.nickname}
-								</div>
-							</div>
-						</div>
-					);
-				})
-			) : (
+		<>
+			<div className="row_center_wrap">
 				<div className="row_center_wrap">
 					{fliteredata &&
 						fliteredata.map((filter, index) => {
@@ -194,7 +118,8 @@ export default function FILTER_CLASS(props) {
 							);
 						})}
 				</div>
-			)}
-		</div>
+				{/* )} */}
+			</div>
+		</>
 	);
 }
