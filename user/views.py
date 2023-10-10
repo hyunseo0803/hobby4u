371572bf43b46b3db0179fbf3903cb6af2c2237f
hyunseo0803 <abcdefghjk5@naver.com>
2023-10-11 01:04:00 +0900

    ✨ FEAT: 사용자 정보 업데이트 및 성과물 불러오기 기능

--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view
-from .models import Member
+from .models import *
 from .serializers import MemberSerializer
 from django.views.generic import View
 from django.http import HttpResponse, JsonResponse
@@ -14,6 +14,7 @@ import requests
 import json
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from django.shortcuts import get_object_or_404
 
 
 load_dotenv()
@@ -73,7 +74,7 @@ def KakaoCallbackView(request):
 
                 try:
                         # 이미 회원 가입되어 있는 사용자인지 확인
-                    member = Member.objects.get(email=email)
+                    member = Member.objects.get(id=id)
                     
                     expires_at = datetime.utcnow() + timedelta(hours=2)
                     payload = {'id': member.id, 'exp': expires_at}
@@ -92,6 +93,7 @@ def KakaoCallbackView(request):
                         id=id,
                         email=email,
                         nickname=nickname,
+                        info="안녕하세요. 많은 관심과 사랑 부탁드려요 !",
                         profileimg=profileimg,
                         provider="kakao",
                         joindate=datetime.today().date()
@@ -127,16 +129,71 @@ def get_user_data(request):
         payload = jwt.decode(jwt_token,SECRET_KEY,ALGORITHM)
         user_id=payload['id']
         member=Member.objects.get(id=user_id)
+        # achive_all=Performance.objects.all()
+        
+        # achive= Performance.objects.filter(id=user_id)
+        # achive_list=[]
+        # for achive_data in achive:
+        #     data={
+        #         'achive_file':achive_data.file is not None,
+        #         'achive_link':achive_data.link is not None
+        #     }
+            
+        #     achive_list.append(data)
+        
+            
         response_data = {
                     'id': member.id,
                     'nickname': member.nickname,
+                    'info': member.info,
                     'email':member.email,
                     'profileImg':member.profileimg
+                    
                 }
         
         return Response(response_data)
     
     
-    
-    
-    
+@api_view(['POST'])
+def save_user_info(request):
+    if request.method=="POST":
+        
+        jwt_token = request.headers.get('Authorization').split(' ')[1]
+        payload = jwt.decode(jwt_token,SECRET_KEY,ALGORITHM)
+        user_id=payload['id']
+        json_data = json.loads(request.POST.get('json'))
+        nickname = json_data.get('nickname')
+        info = json_data.get('info')
+        email = json_data.get('email')
+
+            # 파일 데이터 추출
+        img = request.FILES.get('img')
+        print(img)        
+        member=Member.objects.get(id=user_id)
+        member.profileimg=img
+        member.nickname=nickname
+        member.email=email
+        member.info=info
+        member.save()
+        
+        return Response("success")
+
+@api_view(['POST'])       
+def get_user_achive(request):
+    if request.method =='POST':
+        # print("--------------------------------")
+        jwt_token = request.headers.get('Authorization').split(' ')[1]
+        # print(jwt_token)
+        payload = jwt.decode(jwt_token,SECRET_KEY,ALGORITHM)
+        user_id=payload['id']
+        achive= Performance.objects.filter(id=user_id)
+        achive_list=[]
+        for achive_data in achive:
+            data={
+                'achive_file':achive_data.file is not None,
+                'achive_link':achive_data.link is not None
+            }
+            
+            achive_list.append(data)
+            print(data)
+    return Response(achive_list)
