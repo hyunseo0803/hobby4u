e7a5e3aea9dcc647b032625a45bb02e07983d898
hyunseo0803 <abcdefghjk5@naver.com>
2023-10-11 01:02:53 +0900

    🚚 PATH: 사용자 정보 업데이트 및 성과물 불러오기 경로 추가

--- user/urls.py
+++ user/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('kakao/callback/',views.KakaoCallbackView),
     # path('login/',views.KakaoSignUpView),
     path('get_user_data/',views.get_user_data),
+    path('save_user_info/',views.save_user_info),
+    path('get_user_achive/',views.get_user_achive),
     # path('kakaoLogout/',views.kakao_logout)
     
     # path('/api/check_login_status/',check_login_status),
