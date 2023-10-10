1f59608c4fbfe8b22e99c923aa6f0916d98ba4b7
hyunseo0803 <abcdefghjk5@naver.com>
2023-10-11 01:04:56 +0900

    ✨ FEAT: 사용자 정보 및 성과물 수정 기능 구현
    🐛 BUG: 이미지 첨부 input 태그 보이지 않는 문제 해결 중,,,,

--- frontend/src/pages/Setting.js
+++ frontend/src/pages/Setting.js
@@ -1,6 +1,366 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { IoMailOutline } from "react-icons/io5";
+import { IoMdRibbon, IoMdCreate } from "react-icons/io";
 
 function Setting(props) {
-	return <div>세팅 페이지</div>;
+	const [achive, setAchive] = useState([]);
+
+	const [inputText, setInputText] = useState({
+		email: "",
+		info: "",
+		nickname: "",
+	});
+
+	const [inputImg, setInputImg] = useState("");
+
+	const [inputCheck, setInputCheck] = useState(false);
+
+	const [imagepreview, setImagepreview] = useState("");
+
+	const uploadfile = (e) => {
+		const file = e;
+
+		if (file) {
+			const reader = new FileReader();
+
+			reader.onload = () => {
+				if (file.type.startsWith("image/")) {
+					setImagepreview(reader.result);
+					setInputImg(file);
+				}
+
+				reader.readAsDataURL(file);
+			};
+		}
+	};
+
+	useEffect(() => {
+		getUserData();
+		getUserAchiv();
+	}, []);
+
+	const getUserData = async () => {
+		try {
+			const token = localStorage.getItem("token");
+
+			if (!token) {
+				throw new Error("Token is not available");
+			}
+
+			const response = await fetch(
+				"http://localhost:8000/api/user/get_user_data/",
+				{
+					method: "POST",
+					headers: {
+						Authorization: `Bearer ${token}`, // JWT 토큰을 Authorization header에 포함시킴
+					},
+				}
+			);
+			if (response.ok) {
+				const user = await response.json();
+				setInputText({
+					email: user.email,
+					// img: user.profileImg,
+					nickname: user.nickname,
+					info: user.info,
+				});
+				setInputImg(user.profileImg);
+			} else {
+				// 예외처리
+				throw new Error("Failed to fetch user data");
+			}
+		} catch (error) {
+			// 예외처리
+			throw new Error("Token is not available");
+		}
+	};
+	const getUserAchiv = async () => {
+		try {
+			const token = localStorage.getItem("token");
+
+			if (!token) {
+				throw new Error("Token is not available");
+			}
+
+			const response = await fetch(
+				"http://localhost:8000/api/user/get_user_achive/",
+				{
+					method: "POST",
+					headers: {
+						Authorization: `Bearer ${token}`, // JWT 토큰을 Authorization header에 포함시킴
+					},
+				}
+			);
+			if (response.ok) {
+				const achive = await response.json();
+				setAchive(achive);
+			} else {
+				// 예외처리
+				throw new Error("Failed to fetch user data");
+			}
+		} catch (error) {
+			// 예외처리
+			throw new Error("Token is not available");
+		}
+	};
+
+	const handleSave = async () => {
+		console.log(inputImg);
+
+		setInputCheck(false);
+		try {
+			const token = localStorage.getItem("token");
+			if (!token) {
+				throw new Error("Token is not available");
+			}
+			const dataToSend = {
+				nickname: inputText.nickname,
+				info: inputText.info,
+				email: inputText.email,
+			};
+
+			const formdata = new FormData();
+			formdata.append("json", JSON.stringify(dataToSend));
+			formdata.append("img", inputImg);
+
+			const response = await fetch(
+				"http://localhost:8000/api/user/save_user_info/",
+				{
+					method: "POST",
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+					body: formdata,
+				}
+			);
+
+			if (response.ok) {
+				console.log("Data successfully saved!");
+				// 성공적으로 저장된 경우의 처리
+			} else {
+				// 저장 실패한 경우의 처리
+				console.error("Failed to save data");
+			}
+		} catch (error) {
+			// 오류 처리
+			console.error("Error while saving data:", error);
+		}
+	};
+	const handlechangEdit = () => {
+		setInputCheck(true);
+	};
+
+	return (
+		<div className="wrap">
+			<div style={{ display: "flex", flexDirection: "row", marginBottom: 30 }}>
+				<div
+					style={{
+						justifyContent: "center",
+						textAlign: "center",
+						position: "relative",
+					}}
+				>
+					<div className="user_img">
+						{imagepreview !== "" && inputCheck ? (
+							<img
+								src={imagepreview}
+								style={{
+									objectFit: "contain",
+									width: "100%",
+									height: "100%",
+								}}
+								alt="preview-img"
+							/>
+						) : (
+							<img src={inputImg} alt="" />
+						)}
+					</div>
+					<input
+						accept="image/*"
+						type="file"
+						onChange={(e) => {
+							uploadfile(e.target.files[0]);
+						}}
+					/>
+				</div>
+				<div style={{ margin: 10 }}>
+					<div style={{ display: "flex", flexDirection: "row" }}>
+						<input
+							className="user_nickname"
+							value={inputText.nickname}
+							onChange={(e) =>
+								setInputText({ ...inputText, nickname: e.target.value })
+							}
+							disabled={!inputCheck}
+						/>
+					</div>
+					<div style={{ width: "80vh", height: 100, position: "relative" }}>
+						<input
+							className="user_nickname"
+							style={{
+								width: "90%",
+							}}
+							value={inputText.info}
+							onChange={(e) =>
+								setInputText({ ...inputText, info: e.target.value })
+							}
+							disabled={!inputCheck}
+						/>
+					</div>
+				</div>
+			</div>
+			<div
+				style={{
+					borderTop: "1px solid gray",
+					width: 1000,
+					marginTop: 20,
+					opacity: 0.5,
+				}}
+			></div>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					// backgroundColor: "yellow",
+					padding: 10,
+					position: "relative",
+				}}
+			>
+				<div
+					style={{
+						width: 196,
+						// backgroundColor: "red",
+						height: 35,
+						textAlign: "center",
+						alignItems: "center",
+						justifyContent: "center",
+						display: "flex",
+					}}
+				>
+					<IoMailOutline size={28} />
+				</div>
+				<div
+					style={{
+						width: "80vh",
+						// backgroundColor: "blue",
+						marginLeft: 10,
+						padding: 5,
+					}}
+				>
+					<input
+						className="user_nickname"
+						value={inputText.email}
+						onChange={(e) =>
+							setInputText({ ...inputText, email: e.target.value })
+						}
+						disabled={!inputCheck}
+					/>
+				</div>
+			</div>
+			<div
+				style={{
+					borderTop: "1px solid gray",
+					width: 1000,
+					// margin: 20,
+					opacity: 0.5,
+				}}
+			></div>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					// backgroundColor: "red",
+					padding: 10,
+					position: "relative",
+				}}
+			>
+				<div
+					style={{
+						width: 196,
+						height: 35,
+						textAlign: "center",
+						alignItems: "center",
+						justifyContent: "center",
+						display: "flex",
+					}}
+				>
+					<IoMdRibbon size={30} color="#FFD550" />
+				</div>
+				<div
+					style={{
+						width: "80vh",
+						// backgroundColor: "blue",
+						marginLeft: 10,
+						padding: 5,
+					}}
+				>
+					{achive.length !== 0 ? (
+						achive.map((a, index) => <div key={index}>{a}</div>)
+					) : (
+						<div>등록된 성과물이 없습니다 ! </div>
+					)}
+
+					{inputCheck && <button>등록하기</button>}
+				</div>
+			</div>
+
+			<div
+				style={{
+					borderTop: "1px solid gray",
+					width: 1000,
+					marginBottom: 80,
+					opacity: 0.5,
+				}}
+			></div>
+			{inputCheck !== true ? (
+				<button
+					style={{
+						border: "1px solid #F26B6B",
+						color: "#F26B6B",
+						backgroundColor: "transparent",
+						width: 120,
+						padding: 5,
+						marginBottom: 20,
+						textAlign: "center",
+						borderRadius: 8,
+					}}
+					onClick={handlechangEdit}
+				>
+					정보 수정
+				</button>
+			) : (
+				<button
+					style={{
+						border: "1px solid #F26B6B",
+						color: "#F26B6B",
+						backgroundColor: "transparent",
+						width: 120,
+						padding: 5,
+						marginBottom: 20,
+						textAlign: "center",
+						borderRadius: 8,
+					}}
+					onClick={handleSave}
+				>
+					저장
+				</button>
+			)}
+			<div
+				style={{
+					backgroundColor: "#F26B6B",
+					color: "white",
+					// margin: 50,
+					width: 120,
+					padding: 5,
+					textAlign: "center",
+					borderRadius: 8,
+				}}
+			>
+				회원탈퇴
+			</div>
+		</div>
+	);
 }
+
 export default Setting;
