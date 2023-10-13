5f001dec240bc7120cfea65d270c2845062776c4
hyunseo0803 <abcdefghjk5@naver.com>
2023-10-13 20:26:43 +0900

    FEAT: 링크 유효성 검사 및 빈 값 검사 , 예외처리

--- frontend/src/component/Modal.js
+++ frontend/src/component/Modal.js
@@ -1,18 +1,46 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Modal.css";
+import { BsExclamationTriangle } from "react-icons/bs";
 
 export const Modal = ({ isOpen, onClose, onSave }) => {
-	const [inputLink, setInputLink] = useState("");
+	const [inputTitle, setInputTitle] = useState(""); // 제목 입력 상태
+	const [inputLink, setInputLink] = useState(""); // 링크 입력 상태
+	const [errorMessage, setErrorMessage] = useState("");
 
 	useEffect(() => {
 		if (isOpen) {
+			setInputTitle("");
 			setInputLink("");
+			setErrorMessage("");
 		}
 	}, [isOpen]);
 
+	// const handleTitleChange = (e) => {
+	// 	setInputLink({ ...inputLink, title: e.target.value });
+	// };
+
+	// const handleLinkChange = (e) => {
+	// 	setInputLink({ ...inputLink, link: e.target.value });
+	// };
+
 	const handleSave = () => {
-		onSave(inputLink); // onSave 콜백을 호출하여 이메일 값을 전달
-		onClose();
+		if (inputTitle && inputLink) {
+			if (isValidURL(inputLink)) {
+				onSave({ title: inputTitle, link: inputLink }); // 입력된 데이터를 부모 페이지로 전달
+				setInputTitle(""); // 입력 필드 초기화
+				setInputLink(""); // 입력 필드 초기화
+				onClose(); // 모달을 닫습니다.
+			} else {
+				setErrorMessage("유효하지 않은 링크입니다. ");
+			}
+		} else {
+			setErrorMessage("제목과 링크를 모두 입력해주세요.");
+		}
+	};
+
+	const isValidURL = (url) => {
+		const pattern = /^(http|https):\/\//;
+		return pattern.test(url);
 	};
 
 	return (
@@ -29,11 +57,52 @@ export const Modal = ({ isOpen, onClose, onSave }) => {
 						display: "flex",
 						justifyContent: "center",
 					}}
-					type="link"
+					type="text"
+					placeholder="제목을 입력해주세요"
+					value={inputTitle}
+					onChange={(e) => setInputTitle(e.target.value)}
+				/>
+				<input
+					style={{
+						width: 550,
+						padding: 8,
+						marginTop: 25,
+						marginBottom: 5,
+						border: "1px solid gray",
+						borderRadius: 10,
+						display: "flex",
+						justifyContent: "center",
+					}}
+					type="text"
 					placeholder="링크를 입력해주세요"
 					value={inputLink}
 					onChange={(e) => setInputLink(e.target.value)}
 				/>
+
+				{errorMessage && (
+					<div
+						style={{
+							width: "90%",
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							// justifyContent: "flex-start",
+							// backgroundColor: "yellow",
+						}}
+					>
+						<div
+							style={{
+								// backgroundColor: "yellow",
+								alignItems: "center",
+								display: "flex",
+								margin: 10,
+							}}
+						>
+							<BsExclamationTriangle color="#FD5D5D" size={16} />
+						</div>
+						<div style={{ color: "#fd5d5d", fontSize: 14 }}>{errorMessage}</div>
+					</div>
+				)}
 				<div className="modal-buttons">
 					<button className="buttons" onClick={handleSave}>
 						저장
