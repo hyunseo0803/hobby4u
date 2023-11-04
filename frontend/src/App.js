5b34c5ba111d9af3852d42269aedb46f6fb8634d
hyunseo0803 <abcdefghjk5@naver.com>
2023-11-04 19:20:49 +0900

    ROUTE: 사용자와 관리자 페이지 분리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,16 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
-import Intro from "./pages/Intro";
-import "../src/App.css";
-import Setting from "./pages/Setting";
-import Myclass from "./pages/Myclass";
-import Likeclass from "./pages/Likeclass";
-import Judge from "./pages/Judge";
-import CreateClass from "./pages/CreateClass/CreateClass";
-import CreateClassDetail from "./pages/CreateClass/CreateClassDetail";
-import ReadClass from "./pages/ReadClass/ReadClass";
-import ReadClassDetail from "./pages/ReadClass/ReadClassDetail";
-import SearchClass from "./pages/SerchClass";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserApp from "./UserApp"; // 사용자 페이지 컴포넌트
+import ManagerApp from "./ManagerApp"; // 관리자 페이지 컴포넌트
 
 class App extends Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<div className="Container">
-					<Header />
-					<Routes>
-						<Route path="/" element={<Intro />} />
-						<Route path="intro" element={<Intro />} />
-						<Route path="setting" element={<Setting />} />
-						<Route path="myclass" element={<Myclass />} />
-						<Route path="likeclass" element={<Likeclass />} />
-						<Route path="judge" element={<Judge />} />
-						<Route path="createclass" element={<CreateClass />} />
-						<Route path="createClass/detail" element={<CreateClassDetail />} />
-						<Route path="readClass/readClass" element={<ReadClass />} />
-						<Route path="readClass/classDetail" element={<ReadClassDetail />} />
-						<Route path="readClass/searchClass" element={<SearchClass />} />
-					</Routes>
-
-					<Footer />
-				</div>
+				<Routes>
+					<Route path="/*" element={<UserApp />} />
+					<Route path="/manager/*" element={<ManagerApp />} />
+				</Routes>
 			</BrowserRouter>
 		);
 	}
