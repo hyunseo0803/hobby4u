b2eaec554558a3ec59e911ea5c4d52c070399a1d
hyunseo0803 <abcdefghjk5@naver.com>
2023-11-04 19:31:28 +0900

    STYLES: 구분선 스타일 수정

--- frontend/src/common/HorizonLine.js
+++ frontend/src/common/HorizonLine.js
@@ -1,16 +1,18 @@
 import React from "react";
 //수평 구분선 안의 글자 props
-const HorizonLine = ({ text }) => {
+const HorizonLine = (props) => {
+	const { text } = props;
 	//props 글자의 기본 style
 	const TextStyle = {
 		background: "#fff",
 		padding: "0 10px",
+		color: "gray",
 	};
 
 	const DivStyle = {
 		width: "100%",
 		textAlign: "center",
-		borderBottom: "1px solid #aaa",
+		borderBottom: "1px solid #d3d3d3",
 		lineHeight: "0.1em",
 		margin: "10px 0 20px",
 	};
