dc66197ec605a7d6c6ee6c4994f23cc7aa3aa88d
hyunseo0803 <abcdefghjk5@naver.com>
2023-11-04 19:21:22 +0900

    ROUTE: 관리자 페이지 라우터 생성 및 분리

---
+++ frontend/src/ManagerApp.js
@@ -0,0 +1,196 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+import "../src/App.css";
+
+import JudgeClass from "./pages/adminPage/JudgeClass";
+import SlideBar from "./component/SlideBar";
+import Dash from "./pages/adminPage/Dash";
+import JudgeResult from "./pages/adminPage/JudgeResult";
+import MemberManage from "./pages/adminPage/MemberManage";
+import AdminInfo from "./pages/adminPage/AdminInfo";
+import AdminProfile from "./pages/adminPage/AdminProfile";
+import AdminLogin from "./pages/adminPage/AdminLogin";
+import Approve from "./pages/adminPage/Approve";
+import MAmanage from "./pages/adminPage/MAmanage";
+
+class ManagerApp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoginAdmin: false,
+			adminData: null,
+			notJudgeFinish: "",
+			isJudgeFinish: "",
+			asapJudge: 0,
+		};
+	}
+	async componentDidMount() {
+		const manager = localStorage.getItem("manager");
+
+		if (manager !== null) {
+			await this.getAdminData();
+			await this.finishJudgeCnt();
+			await this.deadlineJudge();
+		}
+		if (!manager) {
+			this.setState({
+				isLoginAdmin: false,
+				adminData: null,
+			});
+			return;
+		}
+	}
+	finishJudgeCnt = async () => {
+		try {
+			const response = await axios.get(
+				"http://localhost:8000/api/manager/get_judge_count/"
+			);
+
+			const notFinishCnt = response.data.notFinshCnt;
+			const FinishCnt = response.data.finishCnt;
+			this.setState({ notJudgeFinish: notFinishCnt, isJudgeFinish: FinishCnt });
+			console.log(this.state);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	deadlineJudge = async () => {
+		try {
+			const response = await axios.get(
+				"http://localhost:8000/api/manager/get_judge_deadline_count/"
+			);
+
+			this.setState({ asapJudge: response.data.deadlineCnt });
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	getAdminData = async () => {
+		try {
+			const manager_token = localStorage.getItem("manager");
+			if (manager_token !== null) {
+				const response = await fetch(
+					"http://localhost:8000/api/manager/get_admin_data/",
+					{
+						method: "POST",
+						headers: {
+							Authorization: `Bearer ${manager_token}`,
+						},
+					}
+				);
+				if (response.ok) {
+					const admin = await response.json();
+
+					this.setState({
+						adminData: admin,
+						isLoginAdmin: true,
+					});
+				} else {
+					// API 호출이 실패, 로그아웃
+					this.setState({
+						adminData: null,
+						isLoginAdmin: false,
+					});
+				}
+			}
+		} catch (error) {
+			// 예외처리
+			console.error(error);
+			this.setState({
+				adminData: null,
+				isLoginAdmin: false,
+			});
+			return;
+		}
+	};
+
+	render() {
+		const {
+			isLoginAdmin,
+			adminData,
+			isJudgeFinish,
+			notJudgeFinish,
+			asapJudge,
+		} = this.state;
+
+		return (
+			<>
+				{isLoginAdmin ? (
+					<div
+						className="Container"
+						style={{ display: "flex", flexDirection: "row" }}
+					>
+						<SlideBar isLoginAdmin={isLoginAdmin} adminData={adminData} />
+
+						<Routes>
+							<Route
+								path="/"
+								element={
+									<Dash
+										adminData={adminData}
+										isJudgeFinish={isJudgeFinish}
+										notJudgeFinish={notJudgeFinish}
+										asapJudge={asapJudge}
+										finishJudgeCnt={this.finishJudgeCnt}
+										deadlineJudge={this.deadlineJudge}
+									/>
+								}
+							/>
+							<Route
+								path="judge/ing/tlatkwnd"
+								element={<JudgeClass adminData={adminData} />}
+							/>
+							<Route
+								path="judge/result/tlatkrufrhk"
+								element={<JudgeResult adminData={adminData} />}
+							/>
+							<Route
+								path="memberAndadmin/wjdqh"
+								element={<MAmanage adminData={adminData} />}
+							/>
+							{/* <Route
+								path="info/wjdqh"
+								element={<AdminInfo adminData={adminData} />}
+							/> */}
+							<Route
+								path="my/wjdqh"
+								element={<AdminProfile adminData={adminData} />}
+							/>
+							<Route
+								path="my/fhrmdkdnt"
+								element={<JudgeClass adminData={adminData} />}
+							/>
+							{adminData.nickname === "메인 관리자" && (
+								<>
+									<Route
+										path="member/ghldnjs"
+										element={<MemberManage adminData={adminData} />}
+									/>
+									<Route
+										path="manager/wjdqh"
+										element={<AdminInfo adminData={adminData} />}
+									/>
+								</>
+							)}
+						</Routes>
+					</div>
+				) : (
+					<>
+						<Routes>
+							<Route
+								path="/"
+								element={<AdminLogin getAdminData={this.getAdminData} />}
+							/>
+							<Route path="approve" element={<Approve />} />
+						</Routes>
+					</>
+				)}
+			</>
+		);
+	}
+}
+
+export default ManagerApp;
---
+++ frontend/src/UserApp.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./component/Header";
+import UserHeader from "./component/UserHeader";
+import Footer from "./component/Footer";
+import Intro from "./pages/Intro";
+import "../src/App.css";
+import Setting from "./pages/Setting";
+import Myclass from "./pages/Myclass";
+import Likeclass from "./pages/Likeclass";
+import Judge from "./pages/Judge";
+import CreateClass from "./pages/CreateClass/CreateClass";
+import CreateClassDetail from "./pages/CreateClass/CreateClassDetail";
+import ReadClass from "./pages/ReadClass/ReadClass";
+import ReadClassDetail from "./pages/ReadClass/ReadClassDetail";
+import SearchClass from "./pages/SerchClass";
+import { IssueClosedIcon } from "@primer/octicons-react";
+import JudgeClass from "./pages/adminPage/JudgeClass";
+
+class UserApp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoggedIn: false,
+			userData: null,
+		};
+	}
+	componentDidMount() {
+		const token = localStorage.getItem("token");
+		if (token) {
+			// 로그인한 경우 사용자 정보를 가져옴
+			this.getUserData();
+		}
+		if (!token) {
+			this.setState({ isLoggedIn: false, userData: null });
+			return;
+		}
+	}
+
+	getUserData = async () => {
+		// if (isLoggedIn) {
+		try {
+			const token = localStorage.getItem("token");
+
+			const response = await fetch(
+				"http://localhost:8000/api/user/get_user_data/",
+				{
+					method: "POST",
+					headers: {
+						Authorization: `Bearer ${token}`, // JWT 토큰을 Authorization header에 포함시킴
+					},
+				}
+			);
+			if (response.ok) {
+				const user = await response.json();
+				// 사용자 정보를 가져온 후 상태를 업데이트하는 콜백 함수
+				this.setState({ userData: user, isLoggedIn: true }, () => {
+					// 상태가 업데이트된 후에 실행되는 로직
+					// 이곳에서 헤더를 다시 렌더링하거나 기타 작업을 수행할 수 있습니다.
+					console.log("User data fetched successfully");
+				});
+			} else {
+				// API 호출이 실패한 경우 에러 처리 로직 추가
+				throw new Error("Failed to fetch user data");
+			}
+		} catch (error) {
+			// 예외처리
+			console.error(error);
+			// 헤더를 로그아웃 상태로 렌더링하거나 기타 작업을 수행할 수 있습니다.
+			this.setState({ userData: null, isLoggedIn: false });
+			return;
+			// }
+		}
+	};
+
+	render() {
+		const { isLoggedIn, userData } = this.state;
+		return (
+			// <BrowserRouter>
+			<div className="Container">
+				{isLoggedIn ? (
+					<UserHeader userData={userData} />
+				) : (
+					<Header getUserData={this.getUserData} />
+				)}
+				<Routes>
+					<Route
+						path="/"
+						element={<ReadClass isLoggedIn={isLoggedIn} userData={userData} />}
+					/>
+					<Route path="intro" element={<Intro />} />
+					<Route path="gide" element={<Intro />} />
+					<Route
+						path="readClass/classDetail"
+						element={<ReadClassDetail isLoggedIn={isLoggedIn} />}
+					/>
+					{isLoggedIn && (
+						<>
+							<Route path="myclass" element={<Myclass userData={userData} />} />
+							<Route
+								path="takingclass"
+								element={<Myclass userData={userData} />}
+							/>
+							<Route path="judge" element={<Judge userData={userData} />} />
+							<Route
+								path="likeclass"
+								element={<Likeclass userData={userData} />}
+							/>
+							<Route path="createclass" element={<CreateClass />} />
+							<Route
+								path="createClass/detail"
+								element={<CreateClassDetail />}
+							/>
+							<Route path="setting" element={<Setting />} />
+						</>
+					)}
+				</Routes>
+
+				<Footer />
+			</div>
+			// </BrowserRouter>
+		);
+	}
+}
+
+export default UserApp;
