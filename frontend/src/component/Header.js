314913e61e5e02951b245a48d6f121bdb7ce82e6
hyunseo0803 <abcdefghjk5@naver.com>
2023-11-12 16:39:06 +0900

    ...

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -14,7 +14,7 @@ function Header(props) {
 	const getCode = async (code) => {
 		try {
 			const response = await fetch(
-				"https://hivehobby4u.netlify.app/api/user/kakao/callback/",
+				"http://localhost:8000/api/user/kakao/callback/",
 				{
 					method: "POST",
 					headers: {
