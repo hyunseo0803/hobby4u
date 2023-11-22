6ec8ef574c3db7853a4676a48aee1d0261dc727c
hyunseo0803 <abcdefghjk5@naver.com>
2023-11-22 15:52:36 +0900

    PATH: 클래스 신청자 조회 및 카운트 업 로직 구현

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -16,14 +16,5 @@ urlpatterns = [
     path('create_goodCount_data/',views.create_goodCount_data),
     path('read_goodCount_data/',views.read_goodCount_data),
     path('process_payment/',views.process_payment),
+    path('apply_class/',views.apply_class)
 
-  
-
-    # path('',views.MemberList.as_view()),
-    # path('<int:pk>/',views.MemberDetail.as_view()),
-    # path('kakao/callback/',views.KakaoCallbackView),
-    # path('kakao/login/',views.KakaoSignUpView),
-    # path('get_user_data/',views.get_user_data)
-    
-    # path('/api/check_login_status/',check_login_status),
-]
